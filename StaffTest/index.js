85a89943122515b6082619f53ceb64f375558f85
Dawid <31769799+wybielak@users.noreply.github.com>
2023-11-14 18:06:01 +0100

    System grania dzwięków

--- StaffTest/index.js
+++ StaffTest/index.js
@@ -14,9 +14,54 @@ let MOUSE = {
     isDown: false
 }
 
+let SPEED = 0.005
+
 let NOTES = ["E6", "D6", "C6", "B5", "A5", "G5", "F5",
-            "E5", "D5", "C5", "B4", "A4", "G4", "F4",
-            "E4", "D4", "C4", "B3", "A3", "G3", "F3"]
+             "E5", "D5", "C5", "B4", "A4", "G4", "F4",
+             "E4", "D4", "C4", "B3", "A3", "G3", "F3"]
+
+let FREQ = [1318.51, 1174.66, 1046.5, 987.767, 880, 783.991, 698.456, 659.255, 587.33, 523.251, 493.883, 436.04, 392.44, 349.228, 329.628, 293.665, 261.626, 246.942, 220, 195.998, 174.614]
+
+let MOVING_NOTES = []
+
+let AUDIO_CONTEXT
+
+class MovingNote {
+    constructor(location) {
+        let index = Math.round(location.y/SPACING * 2)
+        this.frequency = FREQ[index - Math.round(NOTES.length/2)+1]
+        this.note = NOTES[index - Math.round(NOTES.length/2)+1]
+        this.location = {
+            x: location.x,
+            y: index*SPACING * 0.5
+        }
+    }
+
+    draw(ctx) {
+        drawNote(ctx, this.location)
+    }
+
+    play() {
+        if (AUDIO_CONTEXT == null) {
+            AUDIO_CONTEXT = new(AudioContext || webkitAudioContext || window.webkitAudioContext)()
+        }
+
+        let duration = 1
+        let oscylator = AUDIO_CONTEXT.createOscillator()
+        let gainNode = AUDIO_CONTEXT.createGain()
+
+        gainNode.gain.setValueAtTime(0, AUDIO_CONTEXT.currentTime)
+        gainNode.gain.linearRampToValueAtTime(0.4, AUDIO_CONTEXT.currentTime+0.05)
+        gainNode.gain.linearRampToValueAtTime(0, AUDIO_CONTEXT.currentTime+duration)
+
+        oscylator.type = "triangle"
+        oscylator.frequency.value = this.frequency
+        oscylator.start(AUDIO_CONTEXT.currentTime)
+        oscylator.stop(AUDIO_CONTEXT.currentTime+duration)
+        oscylator.connect(gainNode)
+        gainNode.connect(AUDIO_CONTEXT.destination)
+    }
+}
 
 function main() { // główna funkcja
     CANVAS = document.getElementById("myStaff")
@@ -26,8 +71,20 @@ function main() { // główna funkcja
 }
 
 function animate() {
+    updateMovingNotes()
     drawScene()
     window.requestAnimationFrame(animate)
+}
+
+function updateMovingNotes() {
+    for (let i=0; i<MOVING_NOTES.length; i++) {
+        MOVING_NOTES[i].location.x -= SPEED*CANVAS.width
+        if (MOVING_NOTES[i].location.x <= MARGIN_LEFT) {
+            MOVING_NOTES[i].play()
+            MOVING_NOTES.splice(i, 1)
+            i--
+        }
+    }
 }
 
 function addEventListeners() {
@@ -67,13 +124,17 @@ function drawScene() { // rysowanie pięciolinii
     }
 
     let index = Math.round(MOUSE.y/SPACING*2)
-
+    
     let location = {
         x: MARGIN_RIGHT,
         y: index*SPACING * 0.5
     }
 
     drawNote(ctx, location)
+
+    for (let i=0; i<MOVING_NOTES.length; i++) {
+        MOVING_NOTES[i].draw(ctx)
+    }
 
     drawClef(ctx, {x: MARGIN_LEFT, y: CANVAS.height * 0.5})
 }
@@ -125,6 +186,7 @@ function onMouseMove(event) {
 
 function onMouseDown(event) {
     MOUSE.isDown = true
+    MOVING_NOTES.push(new MovingNote({x: MARGIN_RIGHT, y: MOUSE.y}))
 }
 
 function onMouseUp(event) {
