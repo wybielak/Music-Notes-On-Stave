57d2ba524f7afb326a8aafe5c7bc7da2f279a906
kubtet <kubtet1@wp.pl>
2023-12-06 23:49:51 +0100

    Routing added

--- music-notes-on-stave-app/src/app/app.module.ts
+++ music-notes-on-stave-app/src/app/app.module.ts
@@ -4,11 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DescriptionComponent } from './description/description.component';
+import { MelodyMakerComponent } from './melody-maker/melody-maker.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent],
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    DescriptionComponent,
+    MelodyMakerComponent,
+    MainComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
